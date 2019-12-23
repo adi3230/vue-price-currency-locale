84236a5c911cd12177acc6769c811c2f6ae8d032
Aditya Joshi <aditya@lillydoo.com>
2019-12-24 00:14:58 +0100

    chore(setup): initialize files

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,36 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
+  collectCoverage: false,
+  moduleNameMapper: {
+    '^vue$': 'vue/dist/vue.common.js',
+  },
+  roots: [
+    '<rootDir>/tests/unit',
+  ],
+  coverageReporters: [
+    'text-summary',
+  ],
+  moduleFileExtensions: [
+    'js',
+    'vue',
+  ],
+  snapshotSerializers: [
+    'jest-serializer-vue',
+  ],
+  transform: {
+    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest',
+    '.+\\.(css|styl|less|sass|scss|png|jpg|svg|ttf|woff|woff2)$': 'jest-transform-stub',
+  },
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/\\.cache/',
+  ],
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/',
+  ],
+  testMatch: [
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[jt]s?(x)',
+  ],
 };
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
-import App from './App.vue';
+import ProductList from './components/productList';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
+  render: h => h(ProductList),
 }).$mount('#app');
