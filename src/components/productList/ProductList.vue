151ae5b7bdd8a9a3603c8b45c909ae634ff6156d
Aditya Joshi <aditya@lillydoo.com>
2019-12-24 00:17:31 +0100

    feat(demo): load mock products

---
+++ src/components/productList/ProductList.vue
@@ -0,0 +1,49 @@
+<template src="./productList.html"></template>
+
+<script>
+import PriceCurrencyDisplay from '../priceCurrencyDisplay';
+
+export default {
+  name: 'ProductList',
+  components: {
+    PriceCurrencyDisplay,
+  },
+  computed: {
+    locale() {
+      return 'de';
+    },
+    currency() {
+      return 'CHF';
+    },
+  },
+  methods: {
+    getProductItems() {
+      const mockProducts = [
+        {
+          id: 1,
+          title: 'iPad 4 Mini',
+          price: 50001,
+          inventory: 2,
+        },
+        {
+          id: 2,
+          title: 'H&M T-Shirt White',
+          price: 1099,
+          inventory: 10,
+        },
+        {
+          id: 3,
+          title: 'Charli XCX - Sucker CD',
+          price: 1999,
+          inventory: 5,
+        },
+      ];
+      return mockProducts;
+    },
+  },
+};
+</script>
+
+<style scoped src="./productList.scss" lang="scss">
+
+</style>
